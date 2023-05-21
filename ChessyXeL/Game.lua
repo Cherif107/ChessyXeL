612e32662795f596e066f042d12c311e2c6ac729
Cherry <78418245+Cherif107@users.noreply.github.com>
2023-05-21 01:13:47 +0100

    ChessyXeL Release [v1] - Game Changes

--- ChessyXeL/Game.lua
+++ ChessyXeL/Game.lua
@@ -2,6 +2,13 @@ local ClassObject = require 'ChessyXeL.display.object.ClassObject'
 local Object = require 'ChessyXeL.display.object.Object'
 local Mouse = require 'ChessyXeL.input.Mouse'
 local Sprite = require 'ChessyXeL.display.Sprite'
+local Text = require 'ChessyXeL.display.Text'
+
+local function Obj(name)
+    local obj = Object()
+    obj.name = name
+    return obj
+end
 
 ---@class Game a class that contains classes / objects from playstate etc
 local Game = {
@@ -12,9 +19,25 @@ local Game = {
     Lib = ClassObject('openfl.Lib'),
 
     mouse = Mouse(),
+
     boyfriend = Sprite.fromTag('boyfriend'),
     gf = Sprite.fromTag('gf'),
     dad = Sprite.fromTag('dad'),
+
+    timeBar = Sprite.fromTag('timeBar'),
+    timeBarBG = Sprite.fromTag('timeBarBG'),
+    timeTxt = Text.fromTag('timeTxt'),
+
+    healthBar = Sprite.fromTag('healthBar'),
+    healthBarBG = Sprite.fromTag('healthBarBG'),
+
+    scoreTxt = Text.fromTag('scoreTxt'),
+    iconP1 = Sprite.fromTag('iconP1'),
+    iconP2 = Sprite.fromTag('iconP2'),
+
+    playerStrums = Obj('playerStrums'),
+    opponentStrums = Obj('opponentStrums'),
+    strumLineNotes = Obj('strumLineNotes')
 }
 setmetatable(Game, {
     __index = function (t, f)
