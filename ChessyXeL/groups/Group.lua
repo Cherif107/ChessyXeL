ed800f3622df965a7bde56f0376308856b6fd33b
Cherry <78418245+Cherif107@users.noreply.github.com>
2023-07-01 16:41:46 +0100

    V 1.1
    oopsie

--- ChessyXeL/groups/Group.lua
+++ ChessyXeL/groups/Group.lua
@@ -56,7 +56,7 @@ Group.add = Method.PUBLIC(function (group, object, onTop)
         if idx > group.length then
             group.length = idx + 1
         end
-        if object.is(Group) then
+        if not object.is(Group) then
             object.add(onTop)
         end
         if onTop then
@@ -75,7 +75,7 @@ Group.add = Method.PUBLIC(function (group, object, onTop)
     group.members[#group.members+1] = object
     group.length = group.length + 1
 
-    if object.is(Group) then
+    if not object.is(Group) then
         object.add(onTop)
     end
     if onTop then
